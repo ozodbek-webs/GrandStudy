2c29c99dbebf23a8f3f2b7bf17193c6e672f24f4
ozodbek-web <ozodbektursunboyev77@gmail.com>
2023-06-29 18:10:45 +0500

    a

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/images/logo.jpg';
+import logo from '../../assets/images/logo1.png';
 import './style.scss'
 const Navbar = () => {
     return (
